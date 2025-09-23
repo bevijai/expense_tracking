ee0b62e751677b6308ff5790252ffbbd1d9258e9
bevijai <vijai.balaiah2@gmail.com>
2025-09-24 00:14:27 +0800

    test: force-static /ok to bypass SSR

--- app/ok/page.tsx
+++ app/ok/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'error'
-export const revalidate = false
+export const dynamic = 'force-static'
+export const revalidate = 0
 export default function OkPage() {
   return (
     <div style={{ padding: 24 }}>OK</div>
